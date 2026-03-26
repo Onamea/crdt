f2ebe0065d926c2632e9d84ee6466dd1fa2e01ed
Mike O'Bank <mike.obank@pm.me>
2026-03-26 22:29:13 +0530

    identifierToFingerprintedName

--- Identifier.ts
+++ Identifier.ts
@@ -1,9 +1,19 @@
-import type { NameKey, PrimaryKey, PublicKeyDisplay, FingerprintedName } from "@vanice/types"
+import type { NameKey, PrimaryKey, PublicKeyDisplay, FingerprintedName, Name, CryptoName } from "@vanice/types"
 import type { Flavor } from "./lib/utils/Flavor.ts"
 import isArray from "./lib/utils/isArray.ts"
 import isString from "./lib/utils/isString.ts"
 import isObject from "./lib/utils/isObject.ts"
-import { isNameKey, isPrimaryKey, isPublicKeyDisplayByCryptoName, isFingerprintedName } from "@vanice/types"
+import { 
+  isNameKey, 
+  isPrimaryKey, 
+  isPublicKeyDisplayByCryptoName, 
+  isFingerprintedName, 
+  parseNameKey, 
+  primaryKeyToFingerprintedName, 
+  publicKeyToPrimaryKey, 
+  isPublicKeyDisplay, 
+  fromPublicKeyDisplay 
+} from "@vanice/types"
 
 export type IdentityKey = NameKey | PrimaryKey | PublicKeyDisplay
 export type IdentityKeys = IdentityKey[]
@@ -48,4 +58,23 @@ export const areSubKeys = (values: unknown): values is SubKeys => {
 
 export const isIdentifier = (value: unknown): value is Identifier => {
   return isIdentityKey(value) || isFingerprintedName(value)
+}
+
+export const identifierToFingerprintedName = async (identifier: Identifier, name?: Name, cryptoName?: CryptoName): Promise<FingerprintedName> => {
+  if (isFingerprintedName(identifier)) return identifier
+  if (isNameKey(identifier)) {
+    const [primaryKey, name] = parseNameKey(identifier)
+    const [fingerprintedName] = await primaryKeyToFingerprintedName(primaryKey, name)
+    return fingerprintedName
+  }
+  if (isPrimaryKey(identifier) && name !== undefined) {
+    const [fingerprintedName] = await primaryKeyToFingerprintedName(identifier, name)
+    return fingerprintedName
+  }
+  if (isPublicKeyDisplay(identifier) && name !== undefined && cryptoName !== undefined) {
+    const primaryKey = publicKeyToPrimaryKey(cryptoName, fromPublicKeyDisplay(identifier))
+    const [fingerprintedName] = await primaryKeyToFingerprintedName(primaryKey, name)
+    return fingerprintedName
+  }
+  throw new Error("Cannot convert Identifier to FingerprintedName: missing name and/or cryptoName for non-NameKey identifier")
 }
--- test/Identifier.test.ts
+++ test/Identifier.test.ts
@@ -1,7 +1,36 @@
-import { assert } from "@std/assert"
-import { isIdentifier } from "../Identifier.ts"
+import { assert, assertEquals, assertRejects } from "@std/assert"
+import { identifierToFingerprintedName, isIdentifier } from "../Identifier.ts"
+
+import data from "./data.mock.ts"
 
 Deno.test("isIdentifier", () => {
   assert(isIdentifier("Alice⏰⚡️💪"))
   assert(isIdentifier("Ab@5M46V7KKKGT8H3DPBRG7SB9Q19YRWPD6XRWWV3GGYPNUD71MK02"))
+})
+
+Deno.test("identifierToFingerprintedName", async () => {
+  const identifier1 = "Alice⏰⚡️💪"
+  const fingerprintedName1 = await identifierToFingerprintedName(identifier1)
+  assertEquals(fingerprintedName1, "Alice⏰⚡️💪")
+  const identifier2 = "Ab@5M46V7KKKGT8H3DPBRG7SB9Q19YRWPD6XRWWV3GGYPNUD71MK02"
+  const fingerprintedName2 = await identifierToFingerprintedName(identifier2)
+  assertEquals(fingerprintedName2, "Ab🏁☃️☔️🎵✈️🌸🎉🏠")
+})
+
+Deno.test("identifierToFingerprintedName throws for PrimaryKey without name", async () => {
+  const primaryKey = data[0].primaryKey
+  await assertRejects(
+    async () => {
+      await identifierToFingerprintedName(primaryKey)
+    }
+  )
+})
+
+Deno.test("identifierToFingerprintedName throws for PublicKeyDisplay without name/cryptoName", async () => {
+  const publicKeyDisplay = data[4].publicKeyDisplay!
+  await assertRejects(
+    async () => {
+      await identifierToFingerprintedName(publicKeyDisplay, "Name")
+    }
+  )
 })
